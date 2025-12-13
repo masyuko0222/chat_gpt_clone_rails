905d9054045ddbd27482ebe3604cfeb80aed552c
masyuko0222 <masyuko0222@gmail.com>
2025-12-13 13:06:48 +0000

    アバター押下時に、メニュー画面が開く/閉じるように

---
+++ app/javascript/controllers/dropdown_controller.js
@@ -0,0 +1,16 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="dropdown"
+export default class extends Controller {
+  static targets = [ "menu" ]
+  connect() {
+  }
+
+  toggle() {
+    if (this.menuTarget.classList.contains("hidden")) {
+      this.menuTarget.classList.remove("hidden")
+    } else {
+      this.menuTarget.classList.add("hidden")
+    }
+  }
+}
